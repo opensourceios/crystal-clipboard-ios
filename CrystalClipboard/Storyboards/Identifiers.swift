ee40bb6fdbff8aaa703843d453e5d141f5859370
Justin Mazzocchi <jzzocc@gmail.com>
2017-08-29 12:18:49 -0400

    Add sbconstants

---
+++ CrystalClipboard/Storyboards/Identifiers.swift
@@ -0,0 +1,6 @@
+// Auto generated file from SBConstants - any changes may be lost
+
+public enum StoryboardNames : String {
+    case LaunchScreen
+    case Main
+}
--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'cocoapods', '~> 1.3'
 gem 'cocoapods-keys', '~> 2.0'
+gem 'sbconstants', '~> 1.2'
