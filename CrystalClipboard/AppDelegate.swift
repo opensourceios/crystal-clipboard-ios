dfc511c871738bd6aa65d98acffc309afb454a35
Justin Mazzocchi <jzzocc@gmail.com>
2017-08-30 11:59:02 -0400

    Add grace period to HUD

--- CrystalClipboard/AppDelegate.swift
+++ CrystalClipboard/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import CoreData
+import PKHUD
+
+private let HUDGracePeriod: TimeInterval = 0.5
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        PKHUD.sharedHUD.gracePeriod = HUDGracePeriod
         return true
     }
 
