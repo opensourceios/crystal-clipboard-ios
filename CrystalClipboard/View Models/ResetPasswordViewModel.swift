7b3d6249dec01e9489391cecd117248cf9593dcd
Justin Mazzocchi <jzzocc@gmail.com>
2017-09-03 18:19:37 -0400

    Refactoring

--- CrystalClipboard/View Models/ResetPasswordViewModel.swift
+++ CrystalClipboard/View Models/ResetPasswordViewModel.swift
@@ -13,30 +13,41 @@ import Moya
 class ResetPasswordViewModel {
     enum FormError: Error {
         case invalidEmail
+        case response(MoyaError)
     }
     
     // MARK: Inputs
     
-    let email = ValidatingProperty<String, FormError>("") { $0.characters.count > 0 ? .valid : .invalid(.invalidEmail) }
+    let email: ValidatingProperty<String, FormError>
     
-    private(set) lazy var submit: Action<Void, Response, MoyaError> = Action(enabledIf: self.submitEnabled) { [unowned self] _ in
-        return self.provider.reactive.request(.resetPassword(email: self.email.value)).filterSuccessfulStatusCodes()
-    }
+    let submit: Action<Void, Void, FormError>
     
     // MARK: Outputs
     
-    private(set) lazy var successMessage: Signal<String, NoError> = self.submit.values.map { _ in "reset-password.will-receive-email".localized }
+    let successMessage: Signal<String, NoError>
     
-    private(set) lazy var errorMessage: Signal<String, NoError> = self.submit.errors.map { $0.response?.statusCode == 404 ? "reset-password.email-not-found".localized : "reset-password.could-not".localized }
+    let errorMessage: Signal<String, NoError>
     
-    // MARK: Private
-    
-    private let provider: APIProvider
-
-    private lazy var submitEnabled: Property<Bool> = self.email.result.map { !$0.isInvalid }
+    // MARK: Initialization
     
     init(provider: APIProvider) {
-        self.provider = provider
+        email = ValidatingProperty("") { $0.characters.count > 0 ? .valid : .invalid(.invalidEmail) }
         
+        let validInput: Property<String?> = email.result.map { $0.value }
+        
+        submit = Action(unwrapping: validInput) { validEmail in
+            provider.reactive.request(.resetPassword(email: validEmail))
+                .filterSuccessfulStatusCodes()
+                .mapError { FormError.response($0) }
+                .map {_ in Void() }
+        }
+        
+        successMessage = submit.values.map { _ in "reset-password.will-receive-email".localized }
+        errorMessage = submit.errors.map {
+            if case let .response(responseError) = $0, responseError.response?.statusCode == 404 {
+                return "reset-password.email-not-found".localized
+            } else {
+                return "reset-password.could-not".localized }
+        }
     }
 }
