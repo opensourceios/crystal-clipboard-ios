4e7e9a3b69e29c31d0f58e42581878f6625db9c0
Justin Mazzocchi <jzzocc@gmail.com>
2017-09-04 21:28:32 -0400

    That one too

--- CrystalClipboard/View Models/ClipsViewModel.swift
+++ CrystalClipboard/View Models/ClipsViewModel.swift
@@ -37,14 +37,10 @@ class ClipsViewModel {
     
     private let dataProvider: ClipsDataProvider
     private let copyObserver: Signal<Signal<String, NoError>, NoError>.Observer
-    private let token: Lifetime.Token
     
     // MARK: Initialization
     
     init(provider: APIProvider, persistentContainer: NSPersistentContainer) {
-        let (lifetime, token) = Lifetime.make()
-        self.token = token
-        
         dataProvider = ClipsDataProvider(managedObjectContext: persistentContainer.viewContext)
         
         let (signal, observer) = Signal<Signal<String, NoError>, NoError>.pipe()
@@ -54,7 +50,6 @@ class ClipsViewModel {
         let allClipsFetched = MutableProperty(true)
         let initialClipCount = dataProvider.fetchedResultsController.fetchedObjects?.count ?? 0
         let clipCount = dataProvider.fetchedResultsController.reactive.producer(forKeyPath: "fetchedObjects")
-            .take(during: lifetime)
             .map { ($0 as? [ManagedClip])?.count ?? 0 }
         let clipPresence: SignalProducer<ClipsViewModel.ClipsPresent, NoError> = clipCount
             .combineLatest(with: allClipsFetched.producer)
