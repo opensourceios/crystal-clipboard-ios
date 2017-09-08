c4bec7f8fa91bfef5286ec5a671bf6ad52556336
Justin Mazzocchi <jzzocc@gmail.com>
2017-09-08 13:53:33 -0400

    Reorganization

--- CrystalClipboard/Controllers/ClipsViewController.swift
+++ CrystalClipboard/Controllers/ClipsViewController.swift
@@ -18,24 +18,20 @@ class ClipsViewController: UIViewController, PersistentContainerSettable, Provid
     var persistentContainer: NSPersistentContainer!
     var provider: APIProvider!
     
-    private lazy var viewModel: ClipsViewModel! = ClipsViewModel(provider: self.provider, persistentContainer: self.persistentContainer, pageSize: ClipsViewController.pageSize)
+    private lazy var viewModel: ClipsViewModel = ClipsViewModel(provider: self.provider, persistentContainer: self.persistentContainer, pageSize: ClipsViewController.pageSize)
+    private var pageDisplayedObserver: Signal<Int, NoError>.Observer!
+    private var lastPageDisplayed = 0
     @IBOutlet private weak var tableView: UITableView!
-    fileprivate var pageDisplayedObserver: Signal<Int, NoError>.Observer!
-    fileprivate var lastPageDisplayed = 0
     
-    fileprivate static let noClipsView = NoClipsView.fromNib()!
-    fileprivate static let loadingFooterView = LoadingFooterView.fromNib()!
-    fileprivate static let spacingFooterView = SpacingFooterView.fromNib()!
     private static let copiedHUDFlashDelay: TimeInterval = 0.5
     private static let pageSize = 25
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let uiScheduler = UIScheduler()
-        
         // View model inputs
         
+        let (pageDisplayedSignal, pageDisplayedObserver) = Signal<Int, NoError>.pipe()
         let pageWillAppear = reactive.trigger(for: #selector(UIViewController.viewWillAppear(_:)))
             .skip(first: 1)
             .map { [unowned self] in self.lastPageDisplayed }
@@ -43,27 +39,26 @@ class ClipsViewController: UIViewController, PersistentContainerSettable, Provid
             .notifications(forName: .UIApplicationWillEnterForeground)
             .take(during: reactive.lifetime)
             .map { [unowned self] _ in self.lastPageDisplayed }
-        let (pageDisplayedSignal, pageDisplayedObserver) = Signal<Int, NoError>.pipe()
-        self.pageDisplayedObserver = pageDisplayedObserver
+        
         viewModel.pageDisplayed <~ SignalProducer(values: pageDisplayedSignal.skipRepeats(), pageWillAppear, pageWillEnterForeground).flatten(.merge)
+        self.pageDisplayedObserver = pageDisplayedObserver
         
         // View model outputs
         
-        viewModel.textToCopy.observe(on: UIScheduler()).observeValues {
+        tableView.reactive.showNoClipsMessage <~ viewModel.showNoClipsMessage
+        tableView.reactive.showLoadingFooter <~ viewModel.showLoadingFooter
+        
+        let scheduler = UIScheduler()
+        
+        viewModel.textToCopy.observe(on: scheduler).observeValues {
             UIPasteboard.general.string = $0
             UINotificationFeedbackGenerator().notificationOccurred(.success)
             HUD.flash(.labeledSuccess(title: "clips.copied".localized, subtitle: $0), delay: ClipsViewController.copiedHUDFlashDelay)
         }
         
+        viewModel.changeSets.observe(on: scheduler).observeValues { [unowned self] in self.tableView.performUpdates(fromChangeSet: $0) }
+        
         tableView.dataSource = viewModel.dataSource
-        
-        viewModel.changeSets.observe(on: uiScheduler).observeValues { [unowned self] in self.tableView.performUpdates(fromChangeSet: $0) }
-        
-        tableView.reactive.showNoClipsMessage <~ viewModel.showNoClipsMessage
-        tableView.reactive.showLoadingFooter <~ viewModel.showLoadingFooter
-        
-        // Other setup
-        
         tableView.delegate = self
     }
 }
@@ -73,6 +68,12 @@ extension ClipsViewController: UITableViewDelegate {
         lastPageDisplayed = (indexPath.row + 1) / ClipsViewController.pageSize
         pageDisplayedObserver.send(value: lastPageDisplayed)
     }
+}
+
+fileprivate extension ClipsViewController {
+    fileprivate static let noClipsView = NoClipsView.fromNib()!
+    fileprivate static let loadingFooterView = LoadingFooterView.fromNib()!
+    fileprivate static let spacingFooterView = SpacingFooterView.fromNib()!
 }
 
 fileprivate extension Reactive where Base: UITableView {
