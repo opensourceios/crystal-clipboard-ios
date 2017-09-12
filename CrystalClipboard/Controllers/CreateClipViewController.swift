da4d9f88c042a2b8518db2f5af6b419b5525cd9b
Justin Mazzocchi <jzzocc@gmail.com>
2017-09-11 21:52:36 -0400

    Text view with keyboard aware content inset

--- CrystalClipboard/Controllers/CreateClipViewController.swift
+++ CrystalClipboard/Controllers/CreateClipViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import ReactiveSwift
+import ReactiveCocoa
 
 class CreateClipViewController: ModeledViewController<CreateClipViewModel> {
+    
+    @IBOutlet private weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let notificationCenter = NotificationCenter.default.reactive
+        let keyboardHeightChanges = notificationCenter.notifications(forName: .UIKeyboardDidChangeFrame)
+            .take(during: reactive.lifetime)
+            .map { ($0.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? CGRect)?.height ?? 0 }
+        let keyboardHides = notificationCenter.notifications(forName: .UIKeyboardDidHide).map { _ in CGFloat(0) }
+        let keyboardHeight = SignalProducer(values: keyboardHeightChanges, keyboardHides).flatten(.merge)
+        textView.reactive.contentInsetBottom <~ keyboardHeight
+        
+        textView.becomeFirstResponder()
     }
     
     @IBAction func cancelTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+fileprivate extension Reactive where Base: UIScrollView {
+    fileprivate var contentInsetBottom: BindingTarget<CGFloat> {
+        return makeBindingTarget {
+            var edgeInsets = $0.contentInset
+            edgeInsets.bottom = $1
+            $0.contentInset = edgeInsets
+            $0.scrollIndicatorInsets = edgeInsets
+        }
     }
 }
