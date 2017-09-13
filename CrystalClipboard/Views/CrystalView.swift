7c8ee4826a15f6090d629302f0f8f6817e29b8c0
Justin Mazzocchi <jzzocc@gmail.com>
2017-09-13 02:23:44 -0400

    Cleanup

--- CrystalClipboard/Views/CrystalView.swift
+++ CrystalClipboard/Views/CrystalView.swift
@@ -52,20 +52,15 @@ class CrystalView: UIView {
 
 private extension CrystalView {
     private static let yPositionKeyPath = "position.y"
-    private static let backgroundAnimationDuration: CFTimeInterval = 6
     private static let backgroundAnimationKey = "com.jzzocc.crystal-clipboard.crystal-view-background-animation-key"
-    private static let createSeamlessTileView: () -> UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor(patternImage: UIImage(named: "SeamlessAmethyst")!)
-        return view
-    }
+    private static let durationDivisor: CFTimeInterval = 100
     
     private func backgroundAnimation(layer: CALayer) -> CABasicAnimation {
         let animation = CABasicAnimation(keyPath: CrystalView.yPositionKeyPath)
         animation.fromValue = layer.position.y
         animation.toValue = layer.position.y + layer.bounds.height
         animation.repeatCount = Float.infinity
-        animation.duration = CFTimeInterval(layer.bounds.height) / 100.0
+        animation.duration = CFTimeInterval(layer.bounds.height) / CrystalView.durationDivisor
         animation.isRemovedOnCompletion = false
         
         return animation
